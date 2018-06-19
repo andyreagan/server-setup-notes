3a6a4b4784a85d66c87be1ed1825faaeaa87b485
Andy Reagan <areagan@massmutual.com>
2018-06-18 20:34:30 -0400

    Upgrading python.

---
+++ from-linode/upgrade-python.sh
@@ -0,0 +1,7 @@
+apt-get install -y software-properties-common
+$ sudo apt-get update
+$ sudo apt-get install build-essential libpq-dev libssl-dev openssl libffi-dev zlib1g-dev
+$ sudo apt-get install python3-pip python3-dev
+sudo add-apt-repository ppa:jonathonf/python-3.6
+$ sudo apt-get update
+$ sudo apt-get install python3.6
